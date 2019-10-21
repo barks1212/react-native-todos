4e6d71e1611c29385d302007c8f1c1f98c40a3fb
barks1212 <willbarkerwork@gmail.com>
2019-10-21 19:54:02 +0100

    delete on touch

--- App.js
+++ App.js
@@ -17,17 +17,29 @@ export default function App() {
   const addGoalHandler = goalTitle => {
     setCourseGoals(currentGoals => [
       ...currentGoals,
-      { uid: Math.random().toString(), value: goalTitle }
+      { id: Math.random().toString(), value: goalTitle }
     ]);
+  };
+
+  const removeGoalHandler = goalId => {
+    setCourseGoals(currentGoals => {
+      return currentGoals.filter(goal => goal.id !== goalId);
+    });
   };
 
   return (
     <View style={styles.screen}>
       <GoalInput onAddGoal={addGoalHandler} />
       <FlatList
-        keyExtractor={(item, index) => item.uid}
+        keyExtractor={(item, index) => item.id}
         data={courseGoals}
-        renderItem={itemData => <GoalItem title={itemData.item.value} />}
+        renderItem={itemData => (
+          <GoalItem
+            title={itemData.item.value}
+            id={itemData.item.id}
+            onDelete={removeGoalHandler}
+          />
+        )}
       />
     </View>
   );
